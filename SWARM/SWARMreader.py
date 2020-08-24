7aeaaf3d5144d181032fcdcc3dfcc6cac7f4d0cd
akselgra <akselgra@student.matnat.uio.no>
2020-08-24 15:54:46 +0200

    managed to use spacepy

--- SWARM/SWARMreader.py
+++ SWARM/SWARMreader.py
@@ -1,7 +1,20 @@
+"""
+Requires cdf library.
+"""
 import numpy as np
 import matplotlib.pyplot as plt
+#setting path to cdf library
+from getpass import getuser
+usrname = getuser()
 import os
-os.environ["CDF_LIB"] = "~/CDF/lib"
+os.environ["CDF_LIB"] = "/home/" + usrname +  "/Libraries/cdf/cdf/lib"
 from spacepy import pycdf
 
-print("Hello, World!")
+cdf = pycdf.CDF("Data/testdata/testdata.cdf")
+Ne = (cdf["Ne"])
+latitude = np.array(cdf["Latitude"])
+longitude = np.array(cdf["Longitude"])
+time = (cdf["Timestamp"])
+
+plt.plot(time, Ne)
+plt.show()
