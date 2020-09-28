1774dc130c2c404c691a32f6515b9890b37e1c23
akselgra <akselgra@student.matnat.uio.no>
2020-09-28 12:33:25 +0200

    Started case study on polar density movements

--- SWARM/wave_inspector.py
+++ SWARM/wave_inspector.py
@@ -21,10 +21,8 @@ class WaveInspect(SWARMprocess):
     """
 
     def __init__(self, N = int(1e5)):
+        #Initializing data
         data_path = "/home/" + usrname +  "/Documents/Master/Swarm_Data"
-        # cdfA = pycdf.CDF("Data/Sat_A/SW_OPER_EFIA_LP_1B_20131202T101113_20131202T140109_0501.CDF/SW_OPER_EFIA_LP_1B_20131202T101113_20131202T140109_0501_MDR_EFI_LP.cdf")
-        # cdfB = pycdf.CDF("Data/Sswarm-diss.eo.esa.intat_B/SW_OPER_EFIB_LP_1B_20131202T114445_20131202T153609_0501.CDF/SW_OPER_EFIB_LP_1B_20131202T114445_20131202T153609_0501_MDR_EFI_LP.cdf")
-        # cdfC = pycdf.CDF("Data/Sat_C/SW_OPER_EFIC_LP_1B_20131204T094004_20131204T223759_0501.CDF/SW_OPER_EFIC_LP_1B_20131204T094004_20131204T223759_0501_MDR_EFI_LP.cdf")
 
         cdfA_path = data_path + "/Sat_A/SW_OPER_EFIA_LP_1B_20131221T000000_20131221T235959_0501.CDF/SW_OPER_EFIA_LP_1B_20131221T000000_20131221T235959_0501_MDR_EFI_LP.cdf"
         cdfB_path = data_path + "/Sat_B/SW_OPER_EFIB_LP_1B_20131221T000000_20131221T235959_0501.CDF/SW_OPER_EFIB_LP_1B_20131221T000000_20131221T235959_0501_MDR_EFI_LP.cdf"
@@ -33,7 +31,7 @@ class WaveInspect(SWARMprocess):
         cdfA = pycdf.CDF(cdfA_path)
         cdfB = pycdf.CDF(cdfB_path)
         cdfC = pycdf.CDF(cdfC_path)
-
+        #Retrieving data from CDF files.
         self.NeA = cdfA["Ne"][:N]
         self.NeB = cdfB["Ne"][:N]
         self.NeC = cdfC["Ne"][:N]
@@ -50,9 +48,12 @@ class WaveInspect(SWARMprocess):
         self.radB = cdfB["Radius"][:N]
         self.radC = cdfC["Radius"][:N]
 
+        #Setting time to seconds after midnight
         self.seconds = self.stamp_to_sec(cdfA["Timestamp"][:N])
-
+        self.stamps = cdfA["Timestamp"][:N]
+        #finding first indices of pole region
         self.pole_finder()
+
 
 
     def pole_finder(self):
@@ -65,12 +66,65 @@ class WaveInspect(SWARMprocess):
         is_poleB = np.logical_not(self.latB[:init_cut] < 80)
         is_poleC = np.logical_not(self.latC[:init_cut] < 80)
 
+
+
         self.indA = np.where(is_poleA == 1)
         self.indB = np.where(is_poleB == 1)
         self.indC = np.where(is_poleC == 1)
 
 
+    def pole_case_study(self):
+        """
+        Performs a case study of electron density movement
+        in the polar region.
+        """
+
+        NeA = self.NeA[self.indB]
+        NeB = self.NeB[self.indB]
+        NeC = self.NeC[self.indB]
+        seconds = self.seconds[self.indB]
+
+        plt.plot(seconds, NeB)
+        plt.plot(seconds, NeA)
+        plt.plot(seconds, NeC)
+        plt.xlabel("Seconds since midnight")
+        plt.ylabel("Electron density")
+        plt.title("Electron density measurements at polar region")
+        plt.legend(["Sat B", "Sat A", "Sat C"])
+        plt.show()
+
+        AB_pearson = pearsonr(NeB, NeA)[0]
+        print("The pearson correlation coefficient between A and B is %g" % \
+                                                                    AB_pearson)
+        start = self.indB[0][0]
+        stop = self.indB[0][-1]
+
+        BA_shift = self.timeshift_latitude(self.latB, self.latA, start,\
+         stop, shifts = 7500)
+
+        BC_shift = self.timeshift_latitude(self.latB, self.latC, start,\
+         stop, shifts = 7500)
+
+        NeA = self.NeA[start + BA_shift:stop + BA_shift +1]
+        NeC = self.NeC[start + BC_shift:stop + BC_shift +1]
+
+        plt.plot(seconds, NeB)
+        plt.plot(seconds, NeA)
+        plt.plot(seconds, NeC)
+        plt.xlabel("Seconds since midnight")
+        plt.ylabel("Electron density [cm⁻¹]")
+        plt.title("Shifted electron density measurements")
+        plt.legend(["Sat B", "Sat A", "Sat C"])
+        plt.show()
+
+        AB_shift_pearson = pearsonr(NeB, NeA)[0]
+        print("After shifting, the pearson correlation coefficient between A\
+         and B is %g" % AB_shift_pearson)
+
+
+
+
 
 if __name__ == "__main__":
     object = WaveInspect()
-    object.pole_finder()
+    object.pole_case_study()
