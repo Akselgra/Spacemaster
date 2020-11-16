d862bf79299a8c73868d5a545ff0eff7584d695e
akselgra <akselgra@student.matnat.uio.no>
2020-11-16 01:59:50 +0100

    removed old code, added satrun function

--- Synth/datagen.py
+++ Synth/datagen.py
@@ -6,8 +6,7 @@ class SynthDat():
     Class for creating synthetic 2d data that mimics SWARM data.
     """
 
-    def __init__(self, fs = 2, v = 7615, n = 1000, m = 1, amplitude = 2.5e5,\
-    background = 0):
+    def __init__(self, fs = 2, v = 7615, n = 1000):
         """
         Constructor.
         ----------------------------------------
@@ -15,16 +14,11 @@ class SynthDat():
             fs - sampling frequency [Hz]
             v - velocity of satellite [m/s]
             n - size of one grid dimension
-            m - number of tops in data
         """
         self.fs = fs
         self.v = v
         self.dx = v/fs #grid point distance
         self.n = n
-        self.m = m
-        self.amplitude = amplitude
-        self.background = background
-        self.data = np.zeros((n, n))
 
 
     def gauss_curve2d(self, amplitude, x, y, meanx, meany, stdx, stdy):
@@ -34,40 +28,6 @@ class SynthDat():
         first = (x - meanx)**2/(2*stdx**2)
         second = (y - meany)**2/(2*stdy**2)
         return(amplitude*np.exp(-(first + second)))
-
-    def gaussgrid(self, std = False):
-        """
-        Initializes a grid
-        """
-        A = self.amplitude
-
-        xs = np.arange(n)*self.dx
-        ys = np.arange(n)*self.dx
-        Xs, Ys = np.meshgrid(xs, ys)
-
-        data = np.zeros_like(Xs)
-
-        partsize = int(self.n/self.m) #grid points in each top
-        meanx = partsize/2*self.dx # position of tops in  x direction
-        meany = self.n/2*self.dx
-        if std == False:
-            std = meanx/2
-
-        for m in range(self.m): #filling data from gauss_curve2d
-            curr_mean = meanx + partsize*m*self.dx
-            ind0 = partsize*m
-            for i in range(self.n):
-                for j in range(partsize):
-                    x_ind = i
-                    y_ind = ind0 + j
-                    x = Xs[x_ind, y_ind]
-                    y = Ys[x_ind, y_ind]
-                    data[x_ind, y_ind] = self.gauss_curve2d(A, x, y, curr_mean,\
-                                                            meany, std, std)
-        self.Xs = Xs
-        self.Ys = Ys
-        self.data = data
-
 
     def flatfunc(self,A, x, y, width, stepwidth , xpos, ypos):
         """
@@ -89,7 +49,7 @@ class SynthDat():
         else:
             return B
 
-    
+
     def flatfunc_vec(self, A, satpos, width, stepwidth, bobpos):
         """
         Generalized flatfunc for any dimension
@@ -100,164 +60,161 @@ class SynthDat():
             width - width of bubble
             stepwidth - width of incline
             bobpos - array with [x, y, ...] positions of bubble
-        
+
         returns:
             Electron density at satellite position
         """
-        
+
         width_circ = width + stepwidth
         incline = (A)/stepwidth
         r = np.sqrt(np.sum((satpos - bobpos)**2))
-        
+
         if r <= width:
             return A
 
         elif width < r < width_circ:
-            return(-(r- width)*incline + A)
+            return((width - r)*incline + A)
 
         else:
             return 0
-
-    def flatgrid(self, width = True, stepwidth = True):
-        """
-        Initializes a grid with flat bubbles
-        """
-        A = self.amplitude  
-        xs = np.arange(n)*self.dx
-        ys = np.arange(n)*self.dx
-        Xs, Ys = np.meshgrid(xs, ys)
-
-        data = np.zeros_like(Xs)
-
-        partsize = int(self.n/self.m) #grid points in each top
-        meanx = partsize/2*self.dx # position of tops in  x direction
-        meany = self.n/2*self.dx
-        if width:
-            width = meanx/2
-        if stepwidth:
-            stepwidth = width/4
-
-        for m in range(self.m): #filling data from flatfunc
-            curr_mean = meanx + partsize*m*self.dx
-            ind0 = partsize*m
-            for i in range(self.n):
-                for j in range(partsize):
-                    x_ind = i
-                    y_ind = ind0 + j
-                    x = Xs[x_ind, y_ind]
-                    y = Ys[x_ind, y_ind]
-                    data[x_ind, y_ind] = self.flatfunc(A, x, y, width, stepwidth,\
-                                                       curr_mean, meany)
-        self.Xs = Xs
-        self.Ys = Ys
-        self.data = data
-
-
-
-    def rand_flat_grid(self):
-        """
-        Initializes a grid with flat bubbles
-        with random amplitude, width and stepwidth
-        """
-
-        A_base = self.amplitude  
-        xs = np.arange(n)*self.dx
-        ys = np.arange(n)*self.dx
-        Xs, Ys = np.meshgrid(xs, ys)
-
-        data = np.zeros_like(Xs)
-
-        partsize = int(self.n/self.m) #grid points in each top
-        meanx = partsize/2*self.dx # position of tops in  x direction
-        meany = self.n/2*self.dx
-    
-        width_base = meanx/2
-        
-
-        for m in range(self.m): #filling data from flatfunc
-            curr_mean = meanx + partsize*m*self.dx
-            ind0 = partsize*m
-            
-            width = width_base * np.random.uniform(0.2, 1)#randomize variables
-            stepwidth = width/4
-            A = A_base*np.random.uniform(0.2, 1)
-            
-            for i in range(self.n):
-                for j in range(partsize):
-                    x_ind = i
-                    y_ind = ind0 + j
-                    x = Xs[x_ind, y_ind]
-                    y = Ys[x_ind, y_ind]
-                    satpos = np.array([x, y])
-                    bubpos = np.array([curr_mean, meany])
-                    data[x_ind, y_ind] = self.flatfunc_vec(A, satpos, width, stepwidth,\
-                                                       bubpos)
-        self.Xs = Xs
-        self.Ys = Ys
-        self.data = data
-
 
     def add_bubble(self, A, width, stepwidth, bobpos):
         """
         Adds bubble to data from flatfunc_vec method
         """
-        xs = np.arange(n)*obj.dx
-        ys = np.arange(n)*obj.dx
+        if self.data == True:
+            self.data = np.zeros((n, n))
+        xs = np.arange(self.n)*obj.dx
+        ys = np.arange(self.n)*obj.dx
         Xs, Ys = np.meshgrid(xs, ys)
         for i in range(len(self.data)):
             for j in range(len(self.data[i])):
                 self.data[i,j] += obj.flatfunc_vec(A, np.array([Xs[i, j], Ys[i, j]]), width, stepwidth, bobpos)
-                
-    
+
+
     def density(self, r, C = 6.022e23):
         """
         The density of a bubble with a constant electron count.
-        standard of C is set to Avogadros number/10
+        standard of C is set to Avogadros number
         """
         return(3/4 * C/np.pi * 1/(r**3))
-        
 
+
+    def multibob(self, satpos, bobpos, width, stepwidth, A):
+        """
+        Function for summing over multiple flatfunc bubbles.
+        takes:
+            satpos - array with [x, y, ...] positions of satellite
+            bobpos - list of bubble position arrays
+            width - list of widths
+            stepwidth - list of stepwidths
+            A - list of amplitudes
+
+        returns
+            ne - electron density at position
+        """
+        test = len(bobpos) == len(width) == len(stepwidth) == len(A)
+        msg = "Variables in multibob were not of equal length"
+        assert test, msg
+
+        ne = 0
+        for i in range(len(bobpos)):
+            ne = ne + self.flatfunc_vec(A[i], satpos, width[i],\
+                                    stepwidth[i], bobpos[i])
+        return(ne)
+
+    def satrun(self,t0, t1, satpos, satdir, bobpos, bobvel, width, C, diffus):
+        """
+        Generates synthetic data for one satellite.
+        takes:
+            t0 - initial sampling time [s]
+            t1 - final sampling time [s]
+            satpos - array with initial satellite positions [m]
+            satdir - direction of satellite velocity
+            bobpos - list of initial bubble positions [m]
+            bobvel - list of bubble velocities [m/s]
+            width - list of initial bubble widths [m]
+            C - list of total electron content in bubble
+            diffus - time derivative of width [m/s]
+        returns:
+        nes - array with electron density values
+        """
+
+        n = int((t1 - t0)*self.fs) #nr of samples
+        times = np.linspace(t0, t1, n)
+        nes = np.zeros(n)
+        stepwidth = np.zeros_like(width)
+        A = np.zeros_like(width)
+        dt = times[1] - times[0]
+
+        for i in range(n):
+            satpos = satpos + satdir*self.v*dt#update satellite position
+
+            for j in range(len(bobpos)):
+                bobpos[j] = bobpos[j] + bobvel[j]*dt #update bubble positions
+                width[j] = width[j] + diffus[j]*dt #update bubble width
+                stepwidth[j] = width[j]/10
+                A[j] = self.density(width[j], C[j])
+            nes[i] = self.multibob(satpos, bobpos, width, stepwidth, A)
+
+        return(nes)
 
 if __name__ == "__main__":
+    t0 = 0
+    t1 = 400
     fs = 2
+    n = int((t1 - t0)*fs)
     v = 7615
-    n = 500
-    m = 9
-    amplitude = 225000
-    obj = SynthDat(fs = fs, v = v, n = n, m = m, amplitude = amplitude)
-    '''obj.rand_flat_grid()
-    data = obj.data
-    Xs = obj.Xs
-    Ys = obj.Ys'''
-    
-    xs = np.arange(n)*obj.dx
-    ys = np.arange(n)*obj.dx
-    Xs, Ys = np.meshgrid(xs, ys)
-    
-    pos1 = np.array([obj.dx*250, obj.dx*250])
-    width1 = 100*obj.dx
-    stepwidth1 = width1/10
-    A1 = obj.density(width1)
-    
-    
-    pos2 = np.array([obj.dx*69, obj.dx*100])
-    width2 = 40*obj.dx
-    stepwidth2 = width1/5
-    A2 = obj.density(width2)
-    
-    pos3 = np.array([obj.dx*50, obj.dx*360])
-    width3 = 50*obj.dx
-    stepwidth3 = width3/5
-    A3 = obj.density(width3)
-    
-    obj.add_bubble(A1, width1, stepwidth1, pos1)
-    obj.add_bubble(A2, width2, stepwidth2, pos2)
-    obj.add_bubble(A3, width3, stepwidth3, pos3)
-    data = obj.data
+    obj = SynthDat(fs = fs, v = v, n = n)
 
-    plt.pcolormesh(Xs, Ys, data, cmap = "magma")
-    plt.colorbar()
-    plt.show()
 
-    plt.plot(Xs[int(n/2), :], data[int(n/2), :])
+    width = np.array([17,18,19,20])*v
+
+    c = 6.022e23
+    C = np.zeros_like(width)+c
+
+    diffus = np.array([0.01,0.01,0.01,0.01])*v
+
+    bobpos1 = np.array([50, 0])*v
+    bobpos2 = np.array([100, 0])*v
+    bobpos3 = np.array([150, 0])*v
+    bobpos4 = np.array([250, 0])*v
+    bobpos = np.array([bobpos1, bobpos2, bobpos3, bobpos4])
+
+    bobvel1 = np.array([0, 0])*v
+    bobvel2 = np.array([0, 0])*v
+    bobvel3 = np.array([0, 0])*v
+    bobvel4 = np.array([-0.2, 0])*v
+    bobvel = np.array([bobvel1, bobvel2, bobvel3, bobvel4])
+
+    satpos = np.array([0, 0])
+    satdir = np.array([1, 0])
+
+    width = np.array([50])*v
+    C = np.array([c])
+    diffus = np.array([0.1])*v
+    bobpos = np.array([np.array([-500, 0])])*v
+    bobvel = np.array([np.array([10, 0])])*v
+
+    # satpossies = np.linspace(0, t1*v, n)
+    # xs = np.linspace(-500*v, 500*v, 500)
+    # ys = np.linspace(-500*v, 500*v, 500)
+    # Xs, Ys = np.meshgrid(xs, ys)
+    #
+    # data = np.zeros_like(Xs)
+    # for i in range(len(data)):
+    #     for j in range(len(data[i])):
+    #         possies = np.array([Xs[i, j], Ys[i, j]])
+    #         data[i, j] = obj.multibob(possies, bobpos, width, width/5, obj.density(width))
+    #
+    # plt.figure()
+    # plt.pcolormesh(Xs, Ys, data, cmap = "magma")
+    # plt.colorbar()
+    # plt.plot(satpossies, np.zeros_like(satpossies), "r.")
+    # plt.legend(["Satellite path"])
+    # plt.show()
+
+    nes = obj.satrun(t0, t1, satpos, satdir, bobpos, bobvel, width, C, diffus)
+    times = np.linspace(t0, t1, n)
+    plt.plot(times, nes)
     plt.show()
