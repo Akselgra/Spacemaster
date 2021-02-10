82ca5c1c60a779106db2ed386da4a905bb305be2
akselgra <akselgra@student.matnat.uio.no>
2021-02-10 12:21:36 +0100

    test

--- SWARM/test.py
+++ SWARM/test.py
@@ -1,14 +1,5 @@
 import numpy as np
-import matplotlib.pyplot as plt
-from scipy.io import loadmat
-from getpass import getuser
-usrname = getuser()
 
-path = "/home/" + usrname +  "/Documents/Master/Swarm_Data"
-file = path + "/test.mat"
-
-infile = loadmat(file)
-NeA = infile["NeA"][0]
-secondsA = infile["secondsA"][0]
-plt.plot(secondsA, NeA)
-plt.show()
+def f(x):
+    return(f(x))
+print(f(5))
